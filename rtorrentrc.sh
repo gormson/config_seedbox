ccd55983c8272e3dda6cb8dbfd4c88c39d6dd269
gormson <gormson@gmail.com>
2016-02-07 13:08:05 +0100

    Update rtorrentrc.sh

--- rtorrentrc.sh
+++ rtorrentrc.sh
@@ -2,11 +2,8 @@
 
 while IFS="" read -r utilisateur || [[ -n "$utilisateur" ]]
 do
-
-	mkdir /home/"$utilisateur"/torrents/other
-	chown -R "$utilisateur":"$utilisateur" /home/"$utilisateur"/torrents/other
 	
-	mkdir /home/"$utilisateur"/termines/other
-	chown -R "$utilisateur":"$utilisateur" /home/"$utilisateur"/termines/other
+	mkdir /home/"$utilisateur"/termines/vrac/other
+	chown -R "$utilisateur":"$utilisateur" /home/"$utilisateur"/termines/vrac/other
 
 done < "${1}"
