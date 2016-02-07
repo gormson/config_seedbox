ce0c5580e28e13ab460e27f0635271a9ba9aeabc
gormson <gormson@gmail.com>
2016-02-07 13:09:02 +0100

    Update rtorrentrc.sh

--- rtorrentrc.sh
+++ rtorrentrc.sh
@@ -2,7 +2,7 @@
 
 while IFS="" read -r utilisateur || [[ -n "$utilisateur" ]]
 do
-	
+	rm -R /home/"$utilisateur"/termines/other
 	mkdir /home/"$utilisateur"/termines/vrac/other
 	chown -R "$utilisateur":"$utilisateur" /home/"$utilisateur"/termines/vrac/other
 
