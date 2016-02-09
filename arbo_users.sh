254931d5232f09aae9552e0353f0788468e3d08d
gormson <gormson@gmail.com>
2016-02-09 22:44:06 +0100

    Update arbo_users.sh

--- arbo_users.sh
+++ arbo_users.sh
@@ -3,44 +3,54 @@
 while IFS="" read -r utilisateur || [[ -n "$utilisateur" ]]
 do
 	BASEPATH=/home/"$utilisateur"
-	mkdir "$BASEPATH"/torrents/movie_hd
-	mkdir "$BASEPATH"/torrents/movie_vrac
-	mkdir "$BASEPATH"/torrents/movie_kids
-	mkdir "$BASEPATH"/torrents/tv_vf
-	mkdir "$BASEPATH"/torrents/tv_vo
+	if [ -d $BASEPATH ]
+		mkdir "$BASEPATH"/torrents/movie_hd
+		mkdir "$BASEPATH"/torrents/movie_vrac
+		mkdir "$BASEPATH"/torrents/movie_kids
+		mkdir "$BASEPATH"/torrents/tv_vf
+		mkdir "$BASEPATH"/torrents/tv_vo
 
-	chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/torrents/
+		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/torrents/
 
-	mkdir "$BASEPATH"/termines/
-	mkdir "$BASEPATH"/termines/movie_hd
-	mkdir "$BASEPATH"/termines/movie_vrac
-	mkdir "$BASEPATH"/termines/movie_kids
-	mkdir "$BASEPATH"/termines/tv_vf
-	mkdir "$BASEPATH"/termines/tv_vo
-	mkdir "$BASEPATH"/termines/Unsorted
-	mkdir "$BASEPATH"/termines/vrac
+		mkdir "$BASEPATH"/termines/
+		mkdir "$BASEPATH"/termines/movie_hd
+		mkdir "$BASEPATH"/termines/movie_vrac
+		mkdir "$BASEPATH"/termines/movie_kids
+		mkdir "$BASEPATH"/termines/tv_vf
+		mkdir "$BASEPATH"/termines/tv_vo
+		mkdir "$BASEPATH"/termines/Unsorted
+		mkdir "$BASEPATH"/termines/vrac
 
-	chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/termines/
+		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/termines/
+	else
+		echo "Erreur Création Arbo Dossiers"
+	fi
 	
-	mv /home/"$utilisateur"/.rtorrent.rc /home/"$utilisateur"/.rtorrent.rc.bak
-	mv /home/gormson/rtorrent_rc/.rtorrent_"$utilisateur".rc /home/"$utilisateur"/.rtorrent.rc 
+	if [ -f ./scripts/rtorrent_postprocess ]
+	then
+		cp ./scripts/rtorrent_postprocess "$BASEPATH"
+		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent_postprocess
+		chmod a+x "$BASEPATH"/rtorrent_postprocess
+	else
+		echo "Erreur rtorrent-postprocess"
+	fi
 	
-	service "$utilisateur"-rtorrent restart
+	if [ -f ./scripts/rtorrent-postprocessdelete ]
+	then
+		cp ./scripts/rtorrent-postprocessdelete "$BASEPATH"
+		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent-postprocessdelete
+		chmod a+x "$BASEPATH"/rtorrent-postprocessdelete
+	else
+		echo "Erreur rtorrent-postprocessdelete"
+	fi
 	
-	cp ./scripts/rtorrent_postprocess "$BASEPATH"
-	chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent_postprocess
-	chmod +x "$BASEPATH"/rtorrent_postprocess
-	
-	cp ./scripts/rtorrent-postprocessdelete "$BASEPATH"
-	chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent-postprocessdelete
-	chmod +x "$BASEPATH"/rtorrent-postprocessdelete
-	
-	cp ./scripts/hardlink_delete "$BASEPATH"
-	chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/hardlink_delete
-	chmod +x "$BASEPATH"/hardlink_delete
-	
-	cp ./scripts/rtorrent_sniffer "$BASEPATH"/rtorrent_sniffer
-	chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent_sniffer
-	chmod +x "$BASEPATH"/rtorrent_sniffer
+	if [ -f ./scripts/hardlink_delete ]
+	then
+		cp ./scripts/hardlink_delete "$BASEPATH"
+		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/hardlink_delete
+		chmod a+x "$BASEPATH"/hardlink_delete
+	else
+		echo "hardlink_delete"
+	fi
 
 done < "${1}"
