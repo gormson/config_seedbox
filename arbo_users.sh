fb8452daa3547126ded35865489b1cf628dc119b
gormson <gormson@gmail.com>
2016-02-09 23:13:29 +0100

    Update arbo_users.sh

--- arbo_users.sh
+++ arbo_users.sh
@@ -10,8 +10,6 @@ do
 		mkdir "$BASEPATH"/torrents/tv_vf
 		mkdir "$BASEPATH"/torrents/tv_vo
 
-		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/torrents/
-
 		mkdir "$BASEPATH"/termines/
 		mkdir "$BASEPATH"/termines/movie_hd
 		mkdir "$BASEPATH"/termines/movie_vrac
@@ -22,35 +20,35 @@ do
 		mkdir "$BASEPATH"/termines/vrac
 
 		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/termines/
-	else
-		echo "Erreur Création Arbo Dossiers"
-	fi
 	
-	if [ -f ./scripts/rtorrent_postprocess ]
-	then
-		cp ./scripts/rtorrent_postprocess "$BASEPATH"
-		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent_postprocess
-		chmod a+x "$BASEPATH"/rtorrent_postprocess
-	else
-		echo "Erreur rtorrent-postprocess"
-	fi
+		if [ -f ./scripts/rtorrent_postprocess ]
+		then
+			cp ./scripts/rtorrent_postprocess "$BASEPATH"
+			chmod a+x "$BASEPATH"/rtorrent_postprocess
+		else
+			echo "Erreur rtorrent-postprocess"
+		fi
+		
+		if [ -f ./scripts/rtorrent-postprocessdelete ]
+		then
+			cp ./scripts/rtorrent-postprocessdelete "$BASEPATH"
+			chmod a+x "$BASEPATH"/rtorrent-postprocessdelete
+		else
+			echo "Erreur rtorrent-postprocessdelete"
+		fi
+		
+		if [ -f ./scripts/hardlink_delete ]
+		then
+			cp ./scripts/hardlink_delete "$BASEPATH"
+			chmod a+x "$BASEPATH"/hardlink_delete
+		else
+			echo "hardlink_delete"
+		fi
+		
+		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"
 	
-	if [ -f ./scripts/rtorrent-postprocessdelete ]
-	then
-		cp ./scripts/rtorrent-postprocessdelete "$BASEPATH"
-		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/rtorrent-postprocessdelete
-		chmod a+x "$BASEPATH"/rtorrent-postprocessdelete
 	else
-		echo "Erreur rtorrent-postprocessdelete"
-	fi
-	
-	if [ -f ./scripts/hardlink_delete ]
-	then
-		cp ./scripts/hardlink_delete "$BASEPATH"
-		chown -R "$utilisateur":"$utilisateur" "$BASEPATH"/hardlink_delete
-		chmod a+x "$BASEPATH"/hardlink_delete
-	else
-		echo "hardlink_delete"
+		echo "Erreur Création Arbo Dossiers, Skip user"
 	fi
 
 done < "${1}"
