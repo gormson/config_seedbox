0fde7f21799c906a61dbb5ed0e0b8db79dd2b6a0
gormson <gormson@gmail.com>
2016-02-10 11:24:18 +0100

    Update install_cakebox_user.sh

--- install_cakebox_user.sh
+++ install_cakebox_user.sh
@@ -9,24 +9,11 @@
 #                                                                       #
 #########################################################################
 
-#Création du script de mise à jour des droits pour cakebox
-{
-	printf "#!/bin/bash\n"
-	printf "\n"
-	printf "# mise à jour des chmod et du groupe.\n"
-	printf "# chmod 775 pour les dossiers et chmod 664 pour les fichiers (obligatoire)\n"
-	printf "find /home/$USER/termines ! -group web -type f -exec chmod 664 {} \;\n"
-	printf "find /home/$USER/termines ! -group web -type d -exec chmod 775 {} \;\n"
-	printf "find /home/$USER/termines ! -group web -exec chown $USER:web {} \;\n"
-
-	exit 0
-	} > /tmp/file_torrent.sh
-
 groupadd web
 
 while IFS="" read -r utilisateur || [[ -n "$utilisateur" ]]
 do
-	if [ -d /home/"$utilisateur" ]
+	if [ -n "$utilisateur" ] && [ -d /home/"$utilisateur" ]
 	then
 		usermod -aG web "$utilisateur"
 		usermod -aG web www-data
@@ -35,9 +22,7 @@ do
 		find /home/"$utilisateur"/termines/ -type f -exec chmod 664 {} \;
 		find /home/"$utilisateur"/termines/ -type d -exec chmod 775 {} \;
 	
-		cp /tmp/file_torrent.sh /home/"$utilisateur"/.session/file_torrent.sh
+		cp ./scripts/file_torrent.sh /home/"$utilisateur"/.session/file_torrent.sh
 		chmod a+x /home/"$utilisateur"/.session/file_torrent.sh
-		
-		service "$utilisateur"-rtorrent restart
 	
 done < "${1}"
