7e5f61cb038a09a550f8c752fea19562aa31b908
gormson <gormson@gmail.com>
2016-02-10 09:53:27 +0100

    Update replace_script.sh

--- replace_script.sh
+++ replace_script.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+#$1 Fichier utilisateurs
+#$2 nom du script a remplacer se trouvant dans ./scripts
+
 while IFS="" read -r utilisateur || [[ -n "$utilisateur" ]]
 do
 	if [ ! -n "$utilisateur" ] & [ -d /home/"$utilisateur" ]
 	then
-	  cp /home/"
-		cp ./scripts/rtorrent_postprocessdelete /home/"$utilisateur"
-		chown "$utilisateur":"$utilisateur" /home/"$utilisateur"/rtorrent_postprocessdelete
-		chmod a+x /home/"$utilisateur"/rtorrent_postprocessdelete
+	  	if [ -f /home/"$utilisateur"/"$2" ]
+	  	then
+	  		mv /home/"$utilisateur"/"$2" ./backups/"$2"_$utilisateur_$(date "+%Y%m%d_%H%M%S")
+	  		if [ -f ./scripts/"$2" ]
+	  		then
+	  			cp ./scripts/"$2" /home/"$utilisateur"/
+	  			chmod a+x /home/"$utilisateur"/"$2"
+	  			chown "$utilisateur":"$utilisateur" /home/"$utilisateur"/"$2"
+	  		else
+	  			printf "Aucun scripts %b dans ./scripts pour %b" "$2" """$utilisateur"
+	  		fi
+	  	else
+	  		printf "Le script /home/%b/%b n'existe pas" "$utilisateur" "$2"
+	  	fi
 	else
 		echo "utilisateur : $utilisateur inexistant"
 	fi
