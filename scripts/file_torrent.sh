71420d032cb560d13eda950245cd7efb0110acff
gormson <gormson@gmail.com>
2016-02-10 11:54:24 +0100

    Update file_torrent.sh

--- scripts/file_torrent.sh
+++ scripts/file_torrent.sh
@@ -2,8 +2,8 @@
 
 # mise à jour des chmod et du groupe.
 # chmod 775 pour les dossiers et chmod 664 pour les fichiers (obligatoire)
-find /home/$USER/torrents ! -group web -type f -exec chmod 664 {} \;
-find /home/$USER/torrents ! -group web -type d -exec chmod 775 {} \;
-find /home/$USER/torrents ! -group web -exec chown $USER:web {} \;
+find /home/$USER/termines ! -group web -type f -exec chmod 664 {} \;
+find /home/$USER/termines ! -group web -type d -exec chmod 775 {} \;
+find /home/$USER/termines ! -group web -exec chown $USER:web {} \;
 
 exit 0
